664d1f34d63569532c37728ab6fd28b025b20833
interface <interface@interfacedeMac-mini.local>
2024-08-12 15:49:05 +0800

    君父知否

--- internal/knet/balance/blancer.go
+++ internal/knet/balance/blancer.go
@@ -3,8 +3,10 @@ package balance
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
+	"github.com/cloudwego/kitex/internal"
 	"github.com/cloudwego/kitex/pkg/discovery"
 	"github.com/cloudwego/kitex/pkg/loadbalance"
 	transport "github.com/llsw/ikunet/internal/kitex_gen/transport"
@@ -12,13 +14,25 @@ import (
 	kretry "github.com/llsw/ikunet/internal/knet/registry"
 	cmap "github.com/orcaman/concurrent-map"
 	"golang.org/x/exp/rand"
+	"golang.org/x/sync/singleflight"
 )
 
 var (
-	_         loadbalance.Loadbalancer = &Balancer{}
-	pic       loadbalance.Picker       = &picker{}
-	cache     cmap.ConcurrentMap       = cmap.New()
-	whiteList cmap.ConcurrentMap       = cmap.New()
+	_ loadbalance.Loadbalancer = &Balancer{}
+	_ loadbalance.Picker       = &picker{}
+	_ internal.Reusable        = &picker{}
+
+	cache     cmap.ConcurrentMap = cmap.New()
+	whiteList cmap.ConcurrentMap = cmap.New()
+
+	pickPool = sync.Pool{
+		New: newPicker,
+	}
+	sfg singleflight.Group
+)
+
+const (
+	RULE_CACHE_KEY = "RCK"
 )
 
 func init() {
@@ -30,7 +44,17 @@ type picker struct {
 	ruleResolver RuleResolver
 }
 
+// Recycle implements internal.Reusable.
+func (p *picker) Recycle() {
+	pickPool.Put(p)
+}
+
+func newPicker() any {
+	return &picker{}
+}
+
 func (p *picker) Next(ctx context.Context, request interface{}) discovery.Instance {
+	defer p.Recycle()
 	req := request.(*transport.Transport)
 	var ins []discovery.Instance
 	num := 0
@@ -65,21 +89,38 @@ type Balancer struct {
 }
 
 func NewBalancer(endpoints []string, opts ...kretry.Option) (*Balancer, error) {
-	r, err := NewEtcdRuleResolver(endpoints, opts...)
-	if err != nil {
-		return nil, err
+	var (
+		r  interface{}
+		ok bool
+	)
+	if r, ok = cache.Get(RULE_CACHE_KEY); !ok {
+		var err error
+		r, err, _ = sfg.Do(RULE_CACHE_KEY, func() (interface{}, error) {
+			nr, e := NewEtcdRuleResolver(endpoints, opts...)
+			if e != nil {
+				return nil, e
+			}
+			cache.Set(RULE_CACHE_KEY, nr)
+			return nr, nil
+		})
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &Balancer{
-		ruleResolver: r,
+		ruleResolver: r.(RuleResolver),
 	}, nil
 }
 
+// 获取服务发现的拾取器
 func (b *Balancer) GetPicker(dr discovery.Result) loadbalance.Picker {
-	// 每次都要赋值，防止服务发现结果改变了
-	pic.(*picker).dr = &dr
-	pic.(*picker).ruleResolver = b.ruleResolver
-	return pic
+	// 需要池化，否则会有并发问题
+	pk := pickPool.Get().(*picker)
+	pk.dr = &dr
+	pk.ruleResolver = b.ruleResolver
+	return pk
 }
 
 func (b *Balancer) Name() string {
